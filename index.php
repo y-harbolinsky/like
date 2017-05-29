d3c3e1b248e2eb8d8f05b7019bc732e5a772be68
Yaroslav Harbolinsky <greenua14@gmail.com>
2017-05-29 12:17:35 +0300

    Removed "Connected successfully" message

--- index.php
+++ index.php
@@ -26,7 +26,6 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected successfully";
 
 $users = [];
 $sql = "SELECT * FROM `user_likes`";
